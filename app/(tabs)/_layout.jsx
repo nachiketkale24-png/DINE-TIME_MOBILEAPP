7bf80d35d2a8c79e3b9a35da3e9e0f2f6a4cb899
NachiketKale <nachiket.kale24@spit.ac.in>
2026-01-18 23:07:45 +0530

    fix(tabs): correct icon import and names

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,8 +1,7 @@
-import {view,text} from 'react-native';
 import React from 'react'
 import { Tabs } from 'expo-router'
-import { Colors } from '../../assets/css/Colours';
-import Icnicons from 'react-native-vector-icons/Ionicons';
+import Ionicons from '@expo/vector-icons/Ionicons'
+import { Colors } from '../../assets/css/Colours'
 
 const TabLayout = () => {
   return (
@@ -21,12 +20,20 @@ const TabLayout = () => {
         options={{
           title: 'Home',
           tabBarIcon: ({ color }) => (
-            <Icnicons name="home" size={24} color={color} />
+            <Ionicons name="home" size={24} color={color} />
           ),
         }}
       />
-      <Tabs.Screen name="history" options={{ title: 'History'}} />
-      <Tabs.Screen name="profile" options={{ title: 'Profile' }} />
+      <Tabs.Screen name="history" options={{ title: 'History',
+        tabBarIcon: ({ color }) => (
+            <Ionicons name="time" size={24} color={color} />
+          ),
+      }} />
+      <Tabs.Screen name="profile" options={{ title: 'Profile' ,
+        tabBarIcon: ({ color }) => (
+            <Ionicons name="person-sharp" size={24} color={color} />
+          ),
+      }} />
     </Tabs>
   )
 }
